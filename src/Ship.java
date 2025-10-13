a2bbb662044d15e1c76415797c28a8b9f7b32034
Lane Traynor <lane.traynor@msubillings.edu>
2025-10-13 12:19:43 -0600

    Add Ship class implementation with nested Ships class and ship details

---
+++ src/Ship.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ship {
+    public class Ships {
+    public static void main(String[] args) {
+        Map<String, Integer> Ships = new HashMap<>();
+        Ships.put("Carrier", 5);
+        Ships.put("Battleship", 4);
+        Ships.put("Cruiser", 3);
+        Ships.put("Submarine", 3);
+        Ships.put("Destroyer", 2);
+
+        }
+    }
+
+    public boolean IsSunk() {
+        return false;
+    }
+}
