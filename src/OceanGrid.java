e0f55b0f608e18964771e5c1f48766b83de0866f
Jess <jessica.d.wiseman@gmail.com>
2025-10-15 11:02:15 -0600

    Outline for OG, placeholders present

---
+++ src/OceanGrid.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class OceanGrid extends Grid {
+
+    public OceanGrid(int rows, int column) {
+        super();
+    }
+
+    public void assignShip(List<Coordinate> shipCoordinates) {
+        }
+
+    @Override
+    public char symbolFor(CellState state) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'symbolFor'");
+    }
+    }
+    
