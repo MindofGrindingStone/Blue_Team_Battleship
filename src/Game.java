075ea9771394faf5eeb29d1d192c011a28e43c94
Justin King <justin.king6@msubillings.edu>
2025-10-19 10:20:32 -0600

    Forgot TargetGrid in player, whoops

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,7 @@ public class Game {
     }
 
     public void play() { // method is not fully functional, only currently shows the grid of the first player
-        players.get(0).displayOceanGrid(); // temporary line to test grid printing
+        players.get(0).displayGrids(); // temporary line to test grid printing
         ConsoleHelper.getInput("Press Enter to continue...");
     }
 
--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,24 @@
+import java.lang.annotation.Target;
+
 public class Player {
     private String name;
     private OceanGrid oceanGrid;
+    private TargetGrid targetGrid;
 
     public Player(String name) {
         this.name = name;
         this.oceanGrid = new OceanGrid();
+        this.targetGrid = new TargetGrid();
     }
 
     public String getName() {
         return name;
     }
 
-    public void displayOceanGrid() { // temporary method to show the customer the printing of a grid
+    public void displayGrids() { // temporary method to show the customer the printing of a grid
+        targetGrid.print();
+        System.out.println();
+        System.out.println();
         oceanGrid.print();
     }
 
