e19c33c7786e519be4cfa7b319d86d984f4123f5
Carter Lix <carterlix@gmail.com>
2025-10-19 17:41:02 -0600

    Enhance player selection process by validating input for number of players and improving error handling

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,6 @@
+
 import java.util.List;
+
 
 public class Menu {
 
@@ -64,12 +66,41 @@ public class Menu {
         }
     }
 
-    public void chooseNumberOfPlayers() { 
+    public void chooseNumberOfPlayers() {
         /*  this method needs to choose the number of players then create them I think
         also I'm not sure what to do for the tutorial option up above, I am currently calling this
         right before creating a new game to play the tutorial, maybe we need another method to call
         that would create a tutorial player? Let me know what you think or if you feel like you have
         a good idea just throw it in*/
+        
 
+        int numPlayers = 0;
+        while (true) {
+            try {
+                String input = ConsoleHelper.getInput("Enter number of players (1 or 2): ");
+                numPlayers = Integer.parseInt(input);
+                if (numPlayers == 1 || numPlayers == 2) {
+                    break;
+                }
+                System.out.println("Please enter 1 or 2.");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number.");
+            }
+        }
+
+        for (int i = 1; i <= numPlayers; i++) {
+            String name = ConsoleHelper.getInput("Enter name for Player " + i + ": ");
+            Player player = new Player(name);
+            players.add(player);
+        }
+
+        // For a single player game, create an AI opponent
+        if (numPlayers == 1) {
+            Player ai = new Player("Computer");
+            players.add(ai);
+            System.out.println("A computer opponent has been added!");
+        }
     }
+
 }
+
