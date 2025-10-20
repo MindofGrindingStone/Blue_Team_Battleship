0973adb37b088eb01b05e8238546b768f2d1b6f0
Justin King <justin.king6@msubillings.edu>
2025-10-20 11:27:41 -0600

    added a call to player.getname

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,7 @@ public class Game {
 
     public void play() { // method is not fully functional, only currently shows the grid of the first player
         players.get(0).displayGrids(); // temporary line to test grid printing
+        System.out.println("Thanks for showing up " + players.get(0).getName() + "!");
         ConsoleHelper.getInput("Press Enter to continue...");
     }
 
