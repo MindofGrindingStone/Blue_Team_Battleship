52a97d231d9e23ccdde0aa7f833fb3aeb67cd54a
Carter Lix <carterlix@gmail.com>
2025-10-20 12:30:05 -0600

    Enhance displayGrids method to clearly label grid outputs for better readability

--- src/Player.java
+++ src/Player.java
@@ -14,8 +14,12 @@ public class Player {
     }
 
     public void displayGrids() { // temporary method to show the customer the printing of a grid
+        System.out.print("Target Grid");
+        System.out.println();
         targetGrid.print();
         System.out.println();
+        System.out.println();
+        System.out.print("Ocean Grid");
         System.out.println();
         oceanGrid.print();
     }
