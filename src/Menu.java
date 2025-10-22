0f26ae471e68e9c45c8c3125f71f09b21e5fac05
Justin King <justin.king6@msubillings.edu>
2025-10-22 12:32:50 -0600

    Refactor start menu options and add replay functionality

--- src/Menu.java
+++ src/Menu.java
@@ -42,13 +42,13 @@ public class Menu {
             } else {
                 System.out.println("Thanks for playing Battleship! Please choose from the following options:");
                 Integer choice = Character.getNumericValue(
-                        ConsoleHelper.getInput("1. Start New Game\n2. Tutorial\n3. Exit\n4. Replay\nEnter Here: ")
+                        ConsoleHelper.getInput("1. Replay\n2. Start New Game\n3. Tutorial\n4. Exit\nEnter Here: ")
                                 .charAt(0));
                 while (true) {
                     if (choice < 1 || choice > 4) {
                         System.out.println("Invalid choice, please try again.");
                     } else {
-                        chooseStartOption(choice);
+                        chooseStartOption(choice - 1);
                         break;
                     }
                 }
@@ -58,6 +58,10 @@ public class Menu {
 
     private void chooseStartOption(int choice) {
         switch (choice) {
+            case 0:
+                game = new Game(players);
+                game.play();
+                break;
             case 1:
                 players.clear();
                 chooseNumberOfPlayers();
@@ -73,10 +77,6 @@ public class Menu {
             case 3:
                 System.out.println("Exiting the game. Goodbye!");
                 System.exit(0);
-            case 4:
-                game = new Game(players);
-                game.play();
-                break;
             default:
                 System.out.println("Invalid choice.");
         }
