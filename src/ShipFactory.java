b6aab97f7d2db48c2e04be53447cff22c34f595c
Carter Lix <carterlix@gmail.com>
2025-10-27 12:19:15 -0600

    Refactor ShipFactory class: update method signatures and improve structure

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -1,23 +1,29 @@
 
 import java.util.*;
 
+import apple.laf.JRSUIConstants.Direction;
+
 public abstract class ShipFactory {
 
     private List<Ship> ships = new ArrayList<>();
     private Set<Coordinate> occupiedCoordinates = new HashSet<>();
 
     public List<Ship> getShips() {
+        return ships;
     }
 
     private void createShips() {
     }
 
-    protected abstract void coordinate();
+    public abstract Coordinate chooseStartingCoordinate(String name, int length);
 
-    public void chooseStartingCoordinate(String name, int length) {
-
+    protected abstract Direction chooseOrientation();
+    
+    private Set<Coordinate> generateCoordinates(String name, int length){
     }
+    private Set<Coordinate> generateCoordinatesInDirection(Coordinate coord, int length, int x_offset, int y_offset){
+    }
+    
 
-    protected abstract void direction(char dir);
 
 }
