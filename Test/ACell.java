9ea7d3e81d3abbaf9441583b19c43e24625c6377
Justin King <justin.king6@msubillings.edu>
2025-10-30 11:09:36 -0600

    Refactor ACell tests to use CellState directly instead of Cell.CellState

--- Test/ACell.java
+++ Test/ACell.java
@@ -8,7 +8,7 @@ public class ACell {
     @Test
     void testGetState() {
         Cell cell = new Cell();
-        assertEquals(Cell.CellState.EMPTY, cell.getState(),
+        assertEquals(CellState.EMPTY, cell.getState(),
             "New cells should start in the EMPTY state");
     }
 
@@ -17,16 +17,16 @@ public class ACell {
     void testSetState() {
         Cell cell = new Cell();
 
-        cell.setState(Cell.CellState.OCCUPIED);
-        assertEquals(Cell.CellState.OCCUPIED, cell.getState(),
+        cell.setState(CellState.OCCUPIED);
+        assertEquals(CellState.OCCUPIED, cell.getState(),
             "Cell state should change to OCCUPIED");
 
-        cell.setState(Cell.CellState.HIT);
-        assertEquals(Cell.CellState.HIT, cell.getState(),
+        cell.setState(CellState.HIT);
+        assertEquals(CellState.HIT, cell.getState(),
             "Cell state should change to HIT");
 
-        cell.setState(Cell.CellState.MISS);
-        assertEquals(Cell.CellState.MISS, cell.getState(),
+        cell.setState(CellState.MISS);
+        assertEquals(CellState.MISS, cell.getState(),
             "Cell state should change to MISS");
     }
 }
