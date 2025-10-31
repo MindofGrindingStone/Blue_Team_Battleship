35924a741d00d1b2f393314603126a2566b7c77d
Justin King <justin.king6@msubillings.edu>
2025-10-31 11:43:51 -0600

    Add exception handling to set method in ATargetGridDisplay for improved error management

--- Test/ATargetGridDisplay.java
+++ Test/ATargetGridDisplay.java
@@ -2,11 +2,11 @@ import org.junit.jupiter.api.Test;
 
 public class ATargetGridDisplay {
     static class T extends TargetGrid {
-        void set(int r, int c, CellState s) { setCellState(new Coordinate(r, c), s); }
+        void set(int r, int c, CellState s) throws Exception { setCellState(new Coordinate(r, c), s); }
     }
 
     @Test
-    void prints_target() {
+    void prints_target() throws Exception {
         T g = new T();
         g.set(2, 5, CellState.HIT); // C6 
         g.set(9, 9, CellState.MISS); // J10 
