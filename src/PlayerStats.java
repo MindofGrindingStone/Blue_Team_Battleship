f95b841fbe162845abd394c3131d9eddf608065c
CarterLix <carterlix@gmail.com>
2025-11-12 12:14:10 -0700

    Add PlayerStats class to track game statistics - still work to do on this...

---
+++ src/PlayerStats.java
@@ -0,0 +1,20 @@
+public class PlayerStats {
+    private int shotsFired;
+    private int hits;
+    private int misses;
+    private int shipsRemaining;
+    
+    public int getShotsFired() {
+        return shotsFired;
+    }
+    public int getHits() {
+        return hits;
+    }
+    public int getMisses() {
+        return misses;
+    }
+    public int getShipsRemaining() {
+        return shipsRemaining;
+    }
+
+}
